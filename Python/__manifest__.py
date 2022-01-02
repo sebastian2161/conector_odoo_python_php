8e2700fb48508e4102316a00edf5a469bb4b675f
sebastian2161 <sebastiancoll25@gmail.com>
2022-01-01 21:08:57 -0300

    work03

---
+++ Python/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "odoo_api",
+
+    'summary': """
+        Odoo API Conector""",
+
+    'description': """
+        Odoo API XMLRPC
+    """,
+
+    'author': "SNC SOFT SOLUTIONS",
+    'website': "https://www.snc-soft.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [],
+
+    'demo': [],
+}
